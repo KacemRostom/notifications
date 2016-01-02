4a77f7f2789ccd00e6489e683f9444cccac78da7
google.bk_boukraa <google.bk_boukraa@esi.dz>
2016-01-02 13:22:37 +0100

    First commit244rr

--- notifications.php
+++ notifications.php
@@ -4,10 +4,8 @@ echo $_GET["hub_challenge"];
 
 
 $url = 'http://ooredoo.kacemb.com:9654/notifications.php';
-$fields = array(
-	'entry' => file_get_contents( 'php://input' )
-);
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value; }
+
+$entry = json_encode( file_get_contents( 'php://input' ) );
 
 //open connection
 $ch = curl_init();
@@ -15,7 +13,10 @@ $ch = curl_init();
 //set the url, number of POST vars, POST data
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST, count($fields));
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+curl_setopt($ch,CURLOPT_POSTFIELDS, $entry);
 
 //execute post
 $result = curl_exec($ch);
